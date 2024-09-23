db496d0bc166f8a79f5c8cbf08d10317d4cd7b31
Arman-cloud-ai <armansodhi89@gmail.com>
2024-09-23 10:14:32 -0400

    Add files via upload

---
+++ userController.js
@@ -0,0 +1,26 @@
+```javascript
+const User = require('../models/User');
+
+const createUser = async (req, res) => {
+  // Logic to create a new user
+};
+
+const getUserById = async (req, res) => {
+  // Logic to get a user by ID
+};
+
+const updateUser = async (req, res) => {
+  // Logic to update a user
+};
+
+const deleteUser = async (req, res) => {
+  // Logic to delete a user
+};
+
+module.exports = {
+  createUser,
+  getUserById,
+  updateUser,
+  deleteUser,
+};
+```
